4a88f4ef614c3c36aa8a0f98ea3e897e2daab7b9
Christopher Santana <christopher.santana@nav.no>
2025-05-28 12:39:36 +0200

    Bump ktor.

--- src/main/kotlin/no/nav/tms/brannslukning/gui/opprettBeredskapvarsel.kt
+++ src/main/kotlin/no/nav/tms/brannslukning/gui/opprettBeredskapvarsel.kt
@@ -5,7 +5,6 @@ import io.ktor.http.*
 import io.ktor.http.content.*
 import io.ktor.server.application.*
 import io.ktor.server.request.*
-import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import io.ktor.utils.io.*
 import kotlinx.html.*
@@ -32,17 +31,14 @@ fun Route.opprettBeredskapvarsel(alertRepository: AlertRepository) {
             }
 
             post {
-                log.info { "headers names: ${call.request.headers.names()}" }
-                log.info { "content-type: ${call.request.headers[HttpHeaders.ContentType]}" }
-                val parameters = call.receiveText()
-                call.respondText(parameters)
-//                val hendelse = call.tmpHendelseOrNull() ?: TmpBeredskapsvarsel(
-//                    title = parameters.getFormFieldValue(FormInputField.TITLE),
-//                    description = parameters.getFormFieldValue(FormInputField.DESCRIPTION),
-//                    initatedBy = call.user
-//                )
-//                BeredskapvarselCache.putHendelse(hendelse)
-//                call.respondSeeOther("varsel/${hendelse.id}/$teksterEndpoint")
+                val parameters = call.receiveParameters()
+                val hendelse = call.tmpHendelseOrNull() ?: TmpBeredskapsvarsel(
+                    title = parameters.getFormFieldValue(FormInputField.TITLE),
+                    description = parameters.getFormFieldValue(FormInputField.DESCRIPTION),
+                    initatedBy = call.user
+                )
+                BeredskapvarselCache.putHendelse(hendelse)
+                call.respondSeeOther("varsel/${hendelse.id}/$teksterEndpoint")
             }
         }
 
